002c0bf1ef73790e0dd6d8959b0ca58c9e316816
Leidy <Leidy@leidyleon>
2017-05-25 13:55:52 -0500

    cambie los colores de los footer de las demas vistas

--- application/controllers/Administracion.php
+++ application/controllers/Administracion.php
@@ -1,73 +1,71 @@
 <?php
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Administracion extends CI_Controller {
 
+  //public function costruct
+  public function __construct() {
+    parent:: __construct();
+    $this->load->helper(array('url', 'form', 'array', 'html'));
+    $this->load->model(array('PermisosModel', '', ''));
+  }
 
-	//public function costruct
-    public function __construct() {
-        parent:: __construct();
-        $this->load->helper(array('url', 'form', 'array', 'html'));
-        $this->load->model(array('PermisosModel', '',''));
-    }
-    //
-	public function index()
-	{
-            session_start();
-		$this->load->view('administracion');
-	}
-	//llamo vista de formulirio para perfiles
-    public function crearRoles()
-    {
-        $this->load->view('crearPerfiles');
-    }
-    //
-    public function listarPermisos()
-    {
-        $lista = $this->PermisosModel->listar();
-        header('Content-type: application/json; charset=utf-8');
-        echo json_encode($lista);
-    }
-    //
-    public function nuevoRol()
-    {
-        $nombre = $_POST['nombre'];
-        $insert = $this->PermisosModel->insertar($nombre,$_POST['permisos']);
-        header('Content-type: application/json; charset=utf-8');
-        echo json_encode($insert);
-    }
-    //
-    public function listarRoles()
-    {
-        $roles = $this->PermisosModel->listarRoles();
-        header('Content-type: application/json; charset=utf-8');
-        echo json_encode($roles);
-    }
+  //
+  public function index() {
+    session_start();
+    $this->load->view('administracion');
+  }
 
-    //
-    public function listarUsuarios()
-    {
-        $usuarios = $this->PermisosModel->listarUsuarios();
-        header('Content-type: application/json; charset=utf-8');
-        echo json_encode($usuarios);
-    }
-    public function listarPermisosUsuario()
-    {
-        $idusuario = $_POST['idusuario'];
-        $datosP = $this->PermisosModel->listarPermisosUsuarios($idusuario);
-        //print_r($datosUP);
-        header('Content-type: application/json; charset=utf-8');
-        echo json_encode($datosP);
-    }
-public function eliminarUsuario()
-    {
-        $idusuario = $_POST['iduser'];
-        $datosP = $this->PermisosModel->cambiarEstadoUsuarios($idusuario);
-        //print_r($datosP);
-        header('Content-type: application/json; charset=utf-8');
-        echo json_encode($datosP);
-    }
-    
+  //llamo vista de formulirio para perfiles
+  public function crearRoles() {
+    session_start();
+    $this->load->view('crearPerfiles');
+  }
 
+  //
+  public function listarPermisos() {
+    $lista = $this->PermisosModel->listar();
+    header('Content-type: application/json; charset=utf-8');
+    echo json_encode($lista);
+  }
+
+  //
+  public function nuevoRol() {
+    $nombre = $_POST['nombre'];
+    $insert = $this->PermisosModel->insertar($nombre, $_POST['permisos']);
+    header('Content-type: application/json; charset=utf-8');
+    echo json_encode($insert);
+  }
+
+  //
+  public function listarRoles() {
+    $roles = $this->PermisosModel->listarRoles();
+    header('Content-type: application/json; charset=utf-8');
+    echo json_encode($roles);
+  }
+
+  //
+  public function listarUsuarios() {
+    $usuarios = $this->PermisosModel->listarUsuarios();
+    header('Content-type: application/json; charset=utf-8');
+    echo json_encode($usuarios);
+  }
+
+  public function listarPermisosUsuario() {
+    $idusuario = $_POST['idusuario'];
+    $datosP = $this->PermisosModel->listarPermisosUsuarios($idusuario);
+    //print_r($datosUP);
+    header('Content-type: application/json; charset=utf-8');
+    echo json_encode($datosP);
+  }
+
+  public function eliminarUsuario() {
+    $idusuario = $_POST['iduser'];
+    $datosP = $this->PermisosModel->cambiarEstadoUsuarios($idusuario);
+    //print_r($datosP);
+    header('Content-type: application/json; charset=utf-8');
+    echo json_encode($datosP);
+  }
 
 }
--- application/views/crearPerfiles.php
+++ application/views/crearPerfiles.php
@@ -2,7 +2,7 @@
 <html lang="es">
   <head>
     <!-- CSS  -->
-    <link href="stylesheet" href="<?php echo base_url(); ?>iconfont/material-icons.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link href="<?php echo base_url(); ?>css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
     <title> CREAR PERFILES</title>
